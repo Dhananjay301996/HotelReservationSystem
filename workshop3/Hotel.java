c22bc512362ecf0bb9b27ec6ab6452ddbfcd275a
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-08-20 20:06:23 +0530

    Create Hotel name and its rate variable in hotel class

---
+++ workshop3/Hotel.java
@@ -0,0 +1,22 @@
+package workshop3;
+
+public class Hotel {
+	private String name;
+	private int rate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRate() {
+		return rate;
+	}
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+	
+	
+
+}
--- workshop3/HotelReservation.java
+++ workshop3/HotelReservation.java
@@ -1,7 +1,13 @@
 package workshop3;
-
+/**
+ * Hotel reservation program
+ * @author HP
+ *
+ */
 public class HotelReservation {
-
+/*
+ * Welcome message
+ */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel");
 
